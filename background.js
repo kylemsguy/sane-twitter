1df953609271acf345d1a93f93141c03a7c4c5fe
Kyle Zhou <kylezhou2002@hotmail.com>
2019-07-20 18:24:35 -0700

    Remove commented debug statement

--- background.js
+++ background.js
@@ -2,7 +2,6 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
       for (var i = 0; i < details.requestHeaders.length; ++i) {
         if (details.requestHeaders[i].name === 'User-Agent') {
-            // console.log(details.requestHeaders[i]);
             details.requestHeaders[i].value = "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.18";
             break;
         }
